56134f774aebef475aec8cf64c5b551f1ec95210
MarcelloVSilva <mvnota10@gmail.com>
2017-01-20 13:22:11 -0200

    testes home

--- tests/test.js
+++ tests/test.js
@@ -2,12 +2,12 @@ var erro = false;
 var webdriverio = require('webdriverio');
 var options = {
     desiredCapabilities: {
-        browserName: 'firefox',
+        browserName: 'chrome',
     }
 };
 var options_iPhone = {
     desiredCapabilities: {
-        browserName: 'chrome',
+        browserName: 'firefox',
         size: { width: 320, height: 568 }
     }
 };
@@ -24,7 +24,7 @@ var a = webdriverio
     })
 
 var b = webdriverio
-    .remote(options_iPhone)
+    .remote(options)
     .init()
     .url('http://localhost:5000/')
     .getTitle().then(function (title) {
@@ -38,22 +38,77 @@ var c = webdriverio
     .init()
     .url('http://localhost:5000/')
     .click('#btnOP')
-    .waitForExist(500)
-    .getTitle().then(function (title) {
+    .element('#procurando')
+    .getText().then(function (text) {
         console.log('V3')
-        if (title.indexOf('Exemplo WebRTC/AppCVV') == -1)
-            console.log('O titulo esta errado, na tela esta:' + title + ', porem deveria ser: Exemplo WebRTC/AppCVV');
+        if (title.indexOf('Aguarde alguns instantes que um de nossos voluntários já vai te atender.') == -1)
+            console.log('O texto está errado, devia ser: Aguarde alguns instantes que um de nossos voluntários já vai te atender.');
     })
 
+var d = webdriverio
+    .remote(options)
+    .init()
+    .url('http://localhost:5000/')
+    .click('#btnOP')
+    .getUrl().then(function (url) {
+        console.log('V4')
+        if (title.indexOf('http://localhost:5000/o1.html') == -1)
+            console.log('A url esta errada. Deveria ser: http://localhost:5000/o1.html');
+    })
+
+var e = webdriverio
+    .remote(options)
+    .init()
+    .url('http://localhost:5000/')
+    .element('#btnOP')
+    .getText().then(function (text) {
+        console.log('V5')
+        if (title.indexOf('Fale com o CVV') == -1)
+            console.log('O texto está errado, devia ser: Fale com o CVV');
+    })
+
+var f = webdriverio
+    .remote(options)
+    .init()
+    .url('http://localhost:5000/')
+    .element('#btnSobre')
+    .getText().then(function (text) {
+        console.log('V6')
+        if (title.indexOf('Sobre o CVV') == -1)
+            console.log('O texto está errado, devia ser: Sobre o CVV');
+    })
+
+var g = webdriverio
+    .remote(options)
+    .init()
+    .url('http://localhost:5000/')
+    .element('#btnVol')
+    .getText().then(function (text) {
+        console.log('V7')
+        if (title.indexOf('Acesso de voluntários') == -1)
+            console.log('O texto está errado, devia ser: Acesso de voluntários');
+    })
 
 a.then(function () {
     return b.then(function () {
         return c.then(function(){
-            a.end();
-            b.end();
-            c.end();
-            if (erro)
-                process.exit(1)
+            return d.then(function(){
+                return e.then(function(){
+                    return f.then(function(){
+                        return g.then(function(){
+                            a.end();
+                            b.end();
+                            c.end();
+                            d.end();
+                            e.end();
+                            f.end();
+                            g.end();
+                            if (erro)
+                                process.exit(1)
+                        })
+                    })
+                })
+            })
         })
     })
 })
