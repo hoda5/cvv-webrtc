e386cdb6ed12eba89d6c574bd2a60972887ad047
Fernando Tolentino <JoseFernandoTolentino@gmail.com>
2017-01-21 14:11:43 -0200

    testOP

--- tests/testVol.js
+++ tests/testVol.js
@@ -12,11 +12,22 @@ h.run(
     messias.url(h.domain + '/dashboard.html');
     messias.check_dashboard([0, 0, 0, 0], [0, 0, 0, 0]);
     maria.click('#btnVol')
-    maria.check_text({'.demo-content h5': 'Acesso de voluntários'})
+    maria.wait_text({'.demo-content h5': 'Acesso de voluntários'})
     messias.check_dashboard([0, 0, 0, 0], [0, 0, 0, 0]);
 
     maria.click('.btnPassword')
     maria.wait_text({'.demo-content h3': 'Voluntário Teste'}, 5000)
+    messias.check_dashboard([0, 0, 0, 0], [0, 0, 0, 0]);
+
+    maria.click('#btnDisponibilidade');
+    maria.wait_text({'.demo-content h5': 'Informe por quais canais você está se disponibilizando a atender'})
+    messias.check_dashboard([0, 0, 0, 0], [0, 0, 0, 0]);
+
+    maria.click('#btnDisponibilizar');
+    maria.wait_text({'#procurando': 'Esperando que a outra pessoa nos procure'}, 5000)
+    messias.check_dashboard([0, 0, 0, 0], [0, 0, 0, 1]);
+
+    maria.click('.btnHome');
     messias.check_dashboard([0, 0, 0, 0], [0, 0, 0, 0]);
 
     // ana.check_text({
